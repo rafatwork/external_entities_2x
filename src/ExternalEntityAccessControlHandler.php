2237efb8f272b1a78a75d5711d9c2c8378c78009
Raf Philtjens <raf@rafatwork.be>
2018-08-26 10:30:45 +0200

    Hide form fields (when editing) for read-only external entities

--- src/ExternalEntityAccessControlHandler.php
+++ src/ExternalEntityAccessControlHandler.php
@@ -6,6 +6,9 @@ use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\external_entities\Entity\ExternalEntity;
 
 /**
  * Defines a generic access control handler for external entities.
@@ -37,15 +40,46 @@ class ExternalEntityAccessControlHandler extends EntityAccessControlHandler {
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
     $result = parent::checkCreateAccess($account, $context, $entity_bundle);
 
-    /* @var \Drupal\external_entities\ExternalEntityTypeInterface $external_entity_type */
-    $external_entity_type = \Drupal::entityTypeManager()
-      ->getStorage('external_entity_type')
-      ->load($this->entityTypeId);
+    $external_entity_type = $this->getExternalEntityType();
     if ($external_entity_type && $external_entity_type->isReadOnly()) {
       $result = AccessResult::forbidden()->addCacheableDependency($this->entityType);
     }
 
     return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkFieldAccess($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, FieldItemListInterface $items = NULL) {
+    $result = parent::checkFieldAccess($operation, $field_definition, $account, $items);
+
+    // Do not display form fields when the external entity type is read-only,
+    // with the exception of the annotation field (this allows editing the
+    // annotation directly from the external entity form by using, for example,
+    // the Inline Entity Form module).
+    if ($operation === 'edit') {
+      $external_entity_type = $this->getExternalEntityType();
+      if ($external_entity_type && $external_entity_type->isReadOnly() && $field_definition->getName() !== ExternalEntity::ANNOTATION_FIELD) {
+        $result = AccessResult::forbidden()
+          ->addCacheableDependency($this->entityType)
+          ->addCacheableDependency($external_entity_type);
+      }
+    }
+
+    return $result;
+  }
+
+  /**
+   * Get the external entity type this handler is running for.
+   *
+   * @return \Drupal\external_entities\ExternalEntityTypeInterface|bool
+   *   The external entity type config entity object, or FALSE if not found.
+   */
+  protected function getExternalEntityType() {
+    return \Drupal::entityTypeManager()
+      ->getStorage('external_entity_type')
+      ->load($this->entityTypeId);
   }
 
 }
